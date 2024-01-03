93955140420e350082a7bdaacf6d7338d6aa5133
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-01-03 12:47:21 +0300

    Checking the data in the file

--- Task_#9_5/find_bad_marks.py
+++ Task_#9_5/find_bad_marks.py
@@ -1,6 +1,10 @@
 print("Учащиеся с неудовлетворительной отметкой:")
 
 with open('marks.txt', 'r', encoding='UTF-8') as file:
+    if file.read().strip() == '':
+        print("Файл пуст.")
+    else:
+        file.seek(0)
     for line in file:
         surname, name, mark = line.strip().split()
         if int(mark) < 3:
